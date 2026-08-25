badb1d2c637a4ae002281ce450ad49f47555e719
Harshitha M <161758942+harshitham1974@users.noreply.github.com>
2026-08-25 18:39:52 +0530

    fix: Add TestContainer configuration to ShopsphereApplicationTests for context loading

--- src/test/java/com/ecom/shopsphere/ShopsphereApplicationTests.java
+++ src/test/java/com/ecom/shopsphere/ShopsphereApplicationTests.java
@@ -3,10 +3,27 @@ package com.ecom.shopsphere;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class ShopsphereApplicationTests {
+
+	static final PostgreSQLContainer<?> postgres =
+			new PostgreSQLContainer<>("postgres:16-alpine");
+
+	static {
+		postgres.start();
+	}
+
+	@DynamicPropertySource
+	static void configureProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgres::getJdbcUrl);
+		registry.add("spring.datasource.username", postgres::getUsername);
+		registry.add("spring.datasource.password", postgres::getPassword);
+	}
 
 	@Test
 	void contextLoads() {
